0800f3548cb3633baeb6c5f7a28b11d26e41d8f1
sneha1102 <36694331+sneha1102@users.noreply.github.com>
2020-11-07 18:21:36 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -2,9 +2,28 @@ var http = require("http");
 
 const httpServer = http.createServer(handleServer);
 
-
 function handleServer(req, res) {
-  
+  if (req.url === "/welcome") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    //res.writeHead(200).write();
+    res.end("Welcome to Dominos!");
+  } else if (req.url === "/contact") {
+    const obj = {
+      phone: "18602100000",
+      email: "guestcaredominos@jublfood.com"
+    };
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "application/json");
+    //res.writeHead(200).write(JSON.stringify(obj));
+    res.end(JSON.stringify(obj));
+  } else {
+    res.writeHead(404).end("Page not found");
+  }
 }
+
+httpServer.listen(8081, () => {
+  console.log("Listening");
+});
 
 module.exports = httpServer;
